fa70b85105b5fcd41b822564f2c5d871eff7e0d2
aarticodealchemy <aarti.codealchemy@gmail.com>
2026-01-09 10:58:30 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eg4ConnectDemo',
+  webDir: 'www'
+};
+
+export default config;
